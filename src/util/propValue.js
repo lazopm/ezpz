2c860725152df01f2329b9cf2477065261ffc8fa
Pablo Lazo <lazopm@gmail.com>
2017-06-02 04:13:48 -0400

    Memoize exponential function

--- src/util/propValue.js
+++ src/util/propValue.js
@@ -1,4 +1,6 @@
-export const exponential = (value, base = 2) => num => {
+import memoize from 'lodash.memoize';
+
+export const exponential = memoize((value, base = 2) => num => {
     const space =  num === 0 ? 0 : value * Math.pow(base, Math.abs(num-1)); 
     return num > 0 ? num : num * -1;
-};
+});
