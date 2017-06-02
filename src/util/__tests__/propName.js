79a20ac715832e089dc305cfda948d3751de9a50
Pablo Lazo <plazo@usnews.com>
2017-06-02 12:57:05 -0400

    Add initial tests

---
+++ src/util/__tests__/propName.js
@@ -0,0 +1,80 @@
+import { edgeNames, cornerNames } from '../propName';
+
+describe('edge property names', () => {
+    const {
+        top, bottom,
+        left, right,
+        vertical,
+        horizontal,
+    } = edgeNames('x');
+
+    test('base edges', () => {
+        expect(top).toEqual(['x-top']);
+        expect(bottom).toEqual(['x-bottom']);
+        expect(left).toEqual(['x-left']);
+        expect(right).toEqual(['x-right']);
+    });
+
+    test('combination edges', () => {
+        expect(vertical)
+            .toEqual(expect.arrayContaining([
+                ...top,
+                ...bottom
+            ])
+        );
+        expect(horizontal)
+            .toEqual(expect.arrayContaining([
+                ...left, 
+                ...right
+            ])
+        );
+    });
+});
+
+describe('corner property names', () => {
+    const {
+        topLeft, topRight,
+        bottomLeft, bottomRight,
+        top, bottom,
+        left, right,
+    } = cornerNames('x', 'y');
+
+    test('base corners', () => {
+        expect(topLeft)
+            .toEqual(['x-top-left-y']);
+        expect(topRight)
+            .toEqual(['x-top-right-y']);
+        expect(bottomLeft)
+            .toEqual(['x-bottom-left-y']);
+        expect(bottomRight)
+            .toEqual(['x-bottom-right-y']);
+    });
+
+    test('combination corners', () => {
+        expect(top)
+            .toEqual(expect.arrayContaining([
+                ...topLeft,
+                ...topRight,
+            ])
+        );
+        expect(bottom)
+            .toEqual(expect.arrayContaining([
+                ...bottomLeft,
+                ...bottomRight,
+            ])
+        );
+        expect(left)
+            .toEqual(expect.arrayContaining([
+                ...bottomLeft,
+                ...topLeft,
+            ])
+        );
+        expect(right)
+            .toEqual(expect.arrayContaining([
+                ...topRight,
+                ...bottomRight,
+            ])
+        );
+    });
+
+});
---
+++ src/util/__tests__/propValue.js
@@ -0,0 +1,27 @@
+import { exponential } from '../propValue';
+
+describe('exponential', () => {
+    const base2 = exponential(1);
+    const base3 = exponential(1, 3);
+
+    test('0 should be 0', () => {
+        expect(base2(0)).toBe(0);
+        expect(base3(0)).toBe(0);
+    });
+
+    test('base', () => {
+        expect(base2(1)).toBe(1);
+        expect(base2(3)).toBe(4);
+        expect(base3(1)).toBe(1);
+        expect(base3(3)).toBe(9);
+    });
+
+    test('negative values', () => {
+        expect(base2(-1)).toBe(-1);
+        expect(base2(-3)).toBe(-4);
+    });
+
+    test('undefined value should be NaN', () =>
+        expect(isNaN(base2())).toBe(true)
+    );
+});
